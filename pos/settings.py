b02951b9b381379faa1a87cb3fa2c14314701884
Bastonator <edwarddili092@gmail.com>
2024-01-27 22:20:05 +0000

    final touches

--- pos/settings.py
+++ pos/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,8 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
 
 # Application definition
 
